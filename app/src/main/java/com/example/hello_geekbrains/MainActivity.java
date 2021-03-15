271f315c10622e90cdbfd660f28b7c9fcf29ccf2
Rasel579 <Rasel579@gmail.com>
2021-03-15 21:16:52 +0500

    added new layouts and switching between them

--- app/src/main/java/com/example/hello_geekbrains/MainActivity.java
+++ app/src/main/java/com/example/hello_geekbrains/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.hello_geekbrains;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button switchLayoutButton = findViewById(R.id.firstButton);
+
+        switchLayoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.edit_text_layout);
+                Button toCalendarButton = findViewById(R.id.toCalendarButton);
+                toCalendarButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setContentView(R.layout.calendar_layout);
+                        Button backButton = findViewById(R.id.backButton);
+                        backButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                setContentView(R.layout.activity_main);
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 }
